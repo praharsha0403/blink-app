b60d22ad7a71453a37e69a7d518cea4c7db39895
pnelatur@uci.edu <pnelatur@uci.edu>
2025-12-23 19:28:55 +0530

    Add dummy avfft.h for Windows build compatibility

---
+++ .build-support/include/libavcodec/avfft.h
@@ -0,0 +1,6 @@
+// Dummy avfft.h for compatibility with ffmpeg-sys-next
+// The real avfft.h was removed in FFmpeg 7.x
+#ifndef AVCODEC_AVFFT_H
+#define AVCODEC_AVFFT_H
+
+#endif /* AVCODEC_AVFFT_H */
